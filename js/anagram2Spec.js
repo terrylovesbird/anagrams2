89c4b14c2ee2d4d9bc17b706b153c30d997fc2a3
Jonathan Young <jonyoungg@gmail.com>
2017-02-06 13:16:46 -0600

    fix the specs

--- js/anagram2Spec.js
+++ js/anagram2Spec.js
@@ -1,5 +1,10 @@
 var ana = require("./anagram2"),
   dictionary = ["threads", "trashed", "hardest", "hatreds", "hounds"];
 
-console.log(ana.anagramsFor("threads", dictionary) == ["threads", "trashed", "hardest", "hatreds"]);
-console.log(ana.anagramsFor("apple", dictionary) == []);
+console.log(ana.anagramsFor("threads", dictionary).length == 4);
+console.log(ana.anagramsFor("threads", dictionary)[0] == "threads");
+console.log(ana.anagramsFor("threads", dictionary)[1] == "trashed");
+console.log(ana.anagramsFor("threads", dictionary)[2] == "hardest");
+console.log(ana.anagramsFor("threads", dictionary)[3] == "hatreds");
+
+console.log(ana.anagramsFor("apple", dictionary).length == 0);
