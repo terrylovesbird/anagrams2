366725b78d403daf9343a34ad17c682262affe6c
Jonathan Young <jonyoungg@gmail.com>
2017-02-06 13:17:29 -0600

    Update anagram2_spec.rb

--- ruby/anagram2_spec.rb
+++ ruby/anagram2_spec.rb
@@ -2,5 +2,5 @@ require_relative 'anagram2'
 
 dictionary = ["threads", "trashed", "hardest", "hatreds", "hounds"]
 
-anagrams_for("threads", dictionary) == ["threads", "trashed", "hardest", "hatreds"]
-anagrams_for("apple", dictionary) == []
+puts anagrams_for("threads", dictionary) == ["threads", "trashed", "hardest", "hatreds"]
+puts anagrams_for("apple", dictionary) == []
